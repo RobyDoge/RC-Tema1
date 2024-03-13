b2fcb5d9327bbfcd5455f5dafc733a7091af1feb
RobyDoge <robyionut23@gmail.com>
2024-03-13 18:25:01 +0200

    Ex1-Done; Ex2-Done

--- Tema-1/PBB.cs
+++ Tema-1/PBB.cs
@@ -1,133 +1,142 @@
-﻿namespace Tema_1
+﻿namespace Tema_1;
+
+internal class PBB
 {
-    internal class PBB
+    private string? InitialMessage { get; set; }
+
+    public void Run()
     {
-        private bool[,] Matrix { get; set; }
-        private string? Message { get; set; }
-        public void Run()
+        Console.WriteLine("Enter the Message:");
+        InitialMessage = Console.ReadLine();
+        Verify();
+        var matrix = CreateMatrix();
+        PrintMatrix(matrix);
+        var corruptedMessage = CorruptMessage(matrix);
+        FindCorruptedBit(corruptedMessage);
+    }
+
+    private void FindCorruptedBit(string message)
+    {
+        var newMatrix = StringToMatrix(message);
+        var corruptedRow = FindCorruptedRow(newMatrix);
+        var corruptedColumn = FindCorruptedColumn(newMatrix);
+        var corruptedPosition = corruptedRow * 8 + corruptedColumn;
+        Console.WriteLine($"The corrupted bit is at position {corruptedPosition}");
+    }
+
+    private static bool[,] StringToMatrix(string message)
+    {
+        var numRows = (int)Math.Ceiling((double)message.Length / 8);
+        var matrix = new bool[numRows, 8];
+
+        for (var i = 0; i < message.Length; i++)
         {
-            Console.WriteLine("Introdu mesajul");
-            Message = Console.ReadLine();
-            Verify();
-            Matrix = CreateMatrix();
-            PrintMatrix();
-            CorruptMessage();
-            FindCorruptedBit();
+            var row = i / 8;
+            var col = i % 8;
+            matrix[row, col] = message[i] == '1';
+        }
+        //Console.WriteLine("Matrix:");
+        //PrintMatrix(matrix);
+
+        return matrix;
+    }
+
+    private static int FindCorruptedColumn(bool[,] newMatrix)
+    {
+        for (var i = 0; i < newMatrix.GetLength(1); i++)
+            if (newMatrix[newMatrix.GetLength(0) - 1, i] != SumOnColumn(i,newMatrix))
+                return i;
+
+        throw new Exception("No corrupted column found");
+    }
+
+    private static int FindCorruptedRow(bool[,] newMatrix)
+    {
+        for (var i = 0; i < newMatrix.GetLength(0); i++)
+            if (newMatrix[i, 7] != SumOnRow(i,newMatrix))
+                return i;
+
+        throw new Exception("No corrupted row found");
+    }
+
+    private static string CorruptMessage(bool[,] matrix)
+    {
+        var random = new Random();
+        var stringMatrix = MatrixToString(matrix);
+        var position = random.Next(0, stringMatrix.Length - 1);
+        var oppositeChar = stringMatrix[position] == '0' ? '1' : '0';
+        stringMatrix = stringMatrix.Remove(position, 1).Insert(position, oppositeChar.ToString());
+        Console.WriteLine($"Correct corrupted bit's position is: {position}");
+        return stringMatrix;
+    }
+
+    private static string MatrixToString(bool[,] matrix)
+    {
+        var result = "";
+        for (var i = 0; i < matrix.GetLength(0); i++)
+        for (var j = 0; j < matrix.GetLength(1); j++)
+            result += matrix[i, j] ? "1" : "0";
+        return result;
+    }
+
+    private static void PrintMatrix(bool[,] Matrix)
+    {
+        Console.WriteLine("Matrix:");
+        for (var i = 0; i < Matrix.GetLength(0); i++)
+        {
+            for (var j = 0; j < Matrix.GetLength(1); j++) Console.Write(Matrix[i, j] ? "1" : "0");
+            Console.WriteLine();
+        }
+    }
+
+    private bool[,] CreateMatrix()
+    {
+        var matrix = new bool[InitialMessage.Length / 7 + 1, 8];
+        var noRows = 0;
+        var j = 0;
+        foreach (var c in InitialMessage)
+        {
+            if (c == '1')
+                matrix[noRows, j] = true;
+            else matrix[noRows, j] = false;
+
+            if (noRows + 1 == InitialMessage.Length / 7) matrix[noRows + 1, j] = SumOnColumn(j, matrix);
+            ++j;
+            if (j != 7) continue;
+            matrix[noRows, j] = SumOnRow(noRows, matrix);
+            ++noRows;
+            j = 0;
         }
 
-        private void FindCorruptedBit()
-        {
-            var newMatrix = CreateMatrix();
-            var corruptedRow = FindCorruptedRow(newMatrix);
-            var corruptedColumn = FindCorruptedColumn(newMatrix);
-            var corruptedPosition = corruptedRow * 7 + corruptedColumn;
-            Console.WriteLine($"The corrupted bit is at position {corruptedPosition}");
-        }
+        matrix[noRows, 7] = SumOnRow(noRows,matrix);
+        return matrix;
+    }
 
-        private int FindCorruptedColumn(bool[,] newMatrix)
-        {
-            for (int i = 0; i < newMatrix.GetLength(1); i++)
-            {
-                if (newMatrix[newMatrix.GetLength(0) - 1, i] != Matrix[Matrix.GetLength(0) - 1, i])
-                    return i;
-            }
+    private static bool SumOnColumn(int j, bool[,] matrix)
+    {
+        var sum = 0;
+        var aux = matrix.GetLength(0);
+        for (var i = 0; i < matrix.GetLength(0)-1; i++)
+            if (matrix[i, j])
+                ++sum;
+        return sum % 2 != 0;
+    }
 
-            throw new Exception("No corrupted column found");
+    private static bool SumOnRow(int noRow, bool[,] matrix)
+    {
+        var sum = 0;
+        for (var i = 0; i < 7; i++)
+            if (matrix[noRow, i])
+                ++sum;
+        return sum % 2 != 0;
+    }
 
-        }
+    private void Verify()
+    {
+        if (InitialMessage is null) throw new Exception("InitialMessage is null");
+        if (InitialMessage.Length % 7 != 0)
+            throw new Exception($"There Are Missing {7 - InitialMessage.Length % 7} Bits");
 
-        private int FindCorruptedRow(bool[,] newMatrix)
-        {
-            for (var i = 0; i < newMatrix.GetLength(0); i++)
-            {
-                if (newMatrix[i, 7] != Matrix[i, 7])
-                    return i;
-            }
-
-            throw new Exception("No corrupted row found");
-        }
-
-        private void CorruptMessage()
-        {
-            var random = new Random();
-            var position = random.Next(0, Message.Length);
-            var oppositeChar = Message[position] == '0' ? '1' : '0';
-            Message = Message.Remove(position, 1).Insert(position, oppositeChar.ToString());
-            Console.WriteLine($"Correct corrupted bit's position is: {position}");
-        }
-
-        private void PrintMatrix()
-        {
-            Console.WriteLine("Matrix:");
-            for (var i = 0; i < Matrix.GetLength(0); i++)
-            {
-                for (var j = 0; j < Matrix.GetLength(1); j++)
-                {
-                    Console.Write(Matrix[i, j] ? "1" : "0");
-                }
-                Console.WriteLine();
-            }
-        }
-
-        private bool[,] CreateMatrix()
-        {
-            bool[,] matrix = new bool[Message.Length / 7 + 1,8 ];
-            var noRows =0;
-            var j=0;
-            foreach(var c in Message)
-            {
-                if (c == '1')
-                    matrix[noRows, j] = true;
-                else matrix[noRows,j] = false;
-
-                if (noRows+1 == Message.Length / 7)
-                {
-                    matrix[noRows + 1, j] = SumOnColumn( noRows, j,matrix);
-                }
-                ++j;
-                if (j != 7) continue;
-                matrix[noRows, j]=SumOnRow( noRows,matrix);
-                ++noRows;
-                j = 0;
-
-            }
-            matrix[noRows,7] = false;
-            return matrix;
-        }
-
-        private bool SumOnColumn( int noRows, int j, bool[,] matrix)
-        {
-            var sum = 0;
-            for(var i=0; i <= noRows;i++)
-            {
-                if (matrix[i, j])
-                    ++sum;
-            }
-            return sum % 2 != 0;
-        }
-
-        private bool SumOnRow(int noRow, bool[,]matrix)
-        {
-            var sum=0;
-            for(var i=0;i<7;i++)
-            {
-                if (matrix[noRow, i])
-                    ++sum;
-            }
-            return sum % 2!=0;
-
-        }
-
-        private void Verify()
-        {
-            if(Message is null) throw new Exception("Message is null");
-            if (Message.Length % 7 != 0) throw new Exception($"There Are Missing {7-Message.Length % 7} Bits");
-
-            if (Message.Any(bit => bit != '1' && bit != '0'))
-            {
-                throw new Exception("Unknown Characters");
-            }
-
-        }
+        if (InitialMessage.Any(bit => bit != '1' && bit != '0')) throw new Exception("Unknown Characters");
     }
 }
--- Tema-1/RCR.cs
+++ Tema-1/RCR.cs
@@ -8,19 +8,63 @@ namespace Tema_1
 {
     internal class RCR
     {
-        private string? Message { get; set; }
+        private string? InitialMessage { get; set; }
         private string? Polynomial { get; set; }
         public void Run()
         {
             ReadInput();
             var change = Calculate();
+            var sendMessage=ModifyMessage(change);
+            sendMessage = CorruptMessage(sendMessage);
+            VerifyMessage(sendMessage);
+        }
+
+        private void VerifyMessage(string sendMessage)
+        {
+            Console.WriteLine($"Received Message: {sendMessage}");
+            while (sendMessage.Length >= Polynomial.Length)
+            {
+                StringBuilder result = new();
+                for (var i = 0; i < Polynomial.Length; i++)
+                {
+                    var xorResult = sendMessage[i] ^ Polynomial[i];
+                    result.Append(xorResult == 0 ? '0' : '1');
+                }
+                sendMessage = $"{result}{sendMessage[Polynomial.Length..]}";
+                
+                if(sendMessage.Contains('1'))
+                    sendMessage = sendMessage.TrimStart('0');
+                Console.WriteLine($"New Message: {sendMessage}");
+            }
+            Console.WriteLine(sendMessage.Any(bit => bit == '1') ? "Message is corrupted" : "Message is not corrupted");
+        }
+
+        private static string CorruptMessage(string sendMessage)
+        {
+            var random = new Random();
+            var position = random.Next(0, sendMessage.Length - 1);
+            var oppositeChar = sendMessage[position] == '0' ? '1' : '0';
+            sendMessage = sendMessage.Remove(position, 1).Insert(position, oppositeChar.ToString());
+            Console.WriteLine($"Correct corrupted bit's position is: {position}");
+            return sendMessage;
+        }
+
+        private string ModifyMessage(string change)
+        {
+            var neededSymbols = Polynomial.Length-1 - change.Length;
+            if (neededSymbols > 0)
+            {
+                change = new string('0', neededSymbols)+ change;
+            }
+            Console.WriteLine($"InitialMessage after XOR: {change}");
+            return InitialMessage+ change;
         }
 
         private string Calculate()
         {
             Console.WriteLine($"In-between Steps for XOR:");
-            var addedMessage = Message + new string('0', Polynomial.Length - 1);
-            Console.WriteLine($"Message: {addedMessage}");
+            var addedMessage = InitialMessage + new string('0', Polynomial.Length - 1);
+            Console.WriteLine($"Initial Message: {addedMessage}");
             while (addedMessage.Length >= Polynomial.Length)
             {
                 StringBuilder result = new();
@@ -30,18 +74,18 @@ namespace Tema_1
                     result.Append(xorResult == 0 ? '0' : '1');
                 }
                 addedMessage = $"{result}{addedMessage[Polynomial.Length..]}";
-                addedMessage = addedMessage.TrimStart('0');
-                Console.WriteLine($"Message: {addedMessage}");
+
+                if (addedMessage.Contains('1'))
+                    addedMessage = addedMessage.TrimStart('0');
+                Console.WriteLine($"New Message: {addedMessage}");
             }
-
-
             return addedMessage;
         }
 
         private void ReadInput()
         {
-            Console.WriteLine("Enter Message:");
-            Message = Console.ReadLine();
+            Console.WriteLine("Enter InitialMessage:");
+            InitialMessage = Console.ReadLine();
             Console.WriteLine("Enter Polynomial:");
             Polynomial = Console.ReadLine();
             Verify();
@@ -49,8 +93,8 @@ namespace Tema_1
 
         private void Verify()
         {
-            if (Message is null) throw new Exception("Message is null");
-            if (Message.Any(bit => bit != '1' && bit != '0'))
+            if (InitialMessage is null) throw new Exception("InitialMessage is null");
+            if (InitialMessage.Any(bit => bit != '1' && bit != '0'))
             {
                 throw new Exception("Unknown Characters");
             }
@@ -60,7 +104,7 @@ namespace Tema_1
                 throw new Exception("Unknown Characters");
             }
             if(Polynomial.First()=='0') throw new Exception("Polynomial should start with 1");
-            if(Polynomial.Length>Message.Length) throw new Exception("Polynomial length is greater than message length");
+            if(Polynomial.Length>InitialMessage.Length) throw new Exception("Polynomial length is greater than message length");
         }
     }
 }
